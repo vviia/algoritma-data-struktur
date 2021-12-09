7029688223e738c85c664d62b34b7b028bbda904
via aulia <86775678+viaaulia@users.noreply.github.com>
2021-12-09 22:06:42 +0700

    problem 242

---
+++ leetcode/valid-anagram/valid-anagram_test.go
@@ -0,0 +1,39 @@
+package validanagram_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s   string
+	t   string
+	ans bool
+}{
+
+	{"anagram", "nagaram", true},
+	{"rat", "car", false},
+	{"at", "car", false},
+	{"", "", true},
+	{"qwerty", "qwerty", true},
+}
+
+func Test_isAnagram(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("qwerty \n", tc)
+		ast.Equal(tc.ans, isAnagram(tc.s, tc.t), ":%v", tc)
+	}
+}
+
+func Benchmark_isAnagram(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			isAnagram(tc.s, tc.t)
+		}
+	}
+}
