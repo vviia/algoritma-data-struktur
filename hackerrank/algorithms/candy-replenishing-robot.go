e1c1282e6476aca24026afbce195e121d2128362
octavia <86775678+viaaulia@users.noreply.github.com>
2021-12-30 21:37:30 +0700

    algoritma

---
+++ hackerrank/algorithms/candy-replenishing-robot.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+func main() {
+	var N, T int
+	fmt.Scanf("%v %v", &N, &T)
+
+	buf := N
+
+	var result int
+	for i := 0; i < (T - 1); i++ {
+		var c int
+		fmt.Scan(&c)
+
+		buf = buf - c
+		if buf < 5 {
+			result += N - buf
+			buf = N
+		}
+	}
+	fmt.Println(result)
+}
+*/
